a75c163e6b2796f7ffb2e591ba396592fc3d7e3a
tegansims <github email address>
2019-09-03 10:05:07 +0100

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.create(name:"jay-z")
+Song.create(title:"99 Problems", artist_id:1)
+Song.create(title:"crazy in love", artist_id:1)
+
+Artist.create(name:"beyonce")
+Song.create(title:"Single Ladies", artist_id:2)

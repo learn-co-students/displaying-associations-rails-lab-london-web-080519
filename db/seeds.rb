d47b4a6c574ca70f4e749c27f87983b8ff685ad0
george-kirby <georgeskirby@btinternet.com>
2019-09-04 09:21:11 +0100

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.create(name: "Amazons")
+Artist.create(name: "Duke Dumont")
+Artist.create(name: "Tame Impala")
+
+Song.create(title: "Stay With Me", artist_id: 1)
+Song.create(title: "Black Magic", artist_id: 1)
+Song.create(title: "Ultraviolet", artist_id: 1)
+Song.create(title: "Ocean Drive", artist_id: 2)
+Song.create(title: "Less I Know The Better", artist_id: 3)
